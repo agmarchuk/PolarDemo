44a43e900ae2503a4825cb84fffeb0163320c617
Alexander Marchuk <alexander.g.marchuk@gmail.com>
2013-10-06 17:21:52 +0700

    ДОбавились результаты тестирования

--- bigDbTest/Program.cs
+++ bigDbTest/Program.cs
@@ -16,22 +16,20 @@ namespace BigDbTest
             string path = @"D:\home\dev2012\PolarDemo\";
             Console.WriteLine("Hello!");
             //BigSQL bs = new BigSQL(@"Data Source=(LocalDB)\v11.0;AttachDbFilename="+path+"Databases/test20131006.mdf;Integrated Security=True;Connect Timeout=30");
-            BigSQL bs = new BigSQL(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\home\FactographDatabases\test.mdf;Integrated Security=True;Connect Timeout=30");
-
+            BigSQL bs = new BigSQL(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\Home\FactographDatabases\test20131006.mdf;Integrated Security=True;Connect Timeout=30");
             DateTime tt0 = DateTime.Now;
 
             //bs.PrepareToLoad();
-            //bs.Index();
-            //bs.Load(10000000);
+            //bs.Load(100000000);
             //Console.WriteLine("Load ok. duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
-
+            //return;
             //bs.Index();
             //Console.WriteLine("Load ok. duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
 
 
             bs.Test2("randcol>10000 AND randcol<12000");
             Console.WriteLine("duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
-            bs.Test2("randcol>777777770 AND randcol<777779000");
+            bs.Test2("randcol>777777770 AND randcol<777777780");
             Console.WriteLine("duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
             bs.Test2("randcol=777777777");
             Console.WriteLine("duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
