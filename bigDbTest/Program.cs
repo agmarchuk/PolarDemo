1e2483c29d7c1ea6457fd262e9d2abcc002d7485
Alexander Marchuk <alexander.g.marchuk@gmail.com>
2013-10-07 08:15:43 +0700

    Технический коммит

--- bigDbTest/Program.cs
+++ bigDbTest/Program.cs
@@ -19,10 +19,10 @@ namespace BigDbTest
             BigSQL bs = new BigSQL(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\Home\FactographDatabases\test20131006.mdf;Integrated Security=True;Connect Timeout=30");
             DateTime tt0 = DateTime.Now;
 
-            //bs.PrepareToLoad();
-            //bs.Load(100000000);
-            //Console.WriteLine("Load ok. duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
-            //return;
+            bs.PrepareToLoad();
+            bs.Load(1000000000);
+            Console.WriteLine("Load ok. duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
+            return;
             //bs.Index();
             //Console.WriteLine("Load ok. duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
 
