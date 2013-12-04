92dc4d96d2fd214d40736f8553610ff1c7e60059
Alexander Marchuk <alexander.g.marchuk@gmail.com>
2013-12-04 21:36:39 +0700

    Синхронизации. Возможно, пострадала область BinaryTree

--- BigDBSorting/Program.cs
+++ BigDBSorting/Program.cs
@@ -10,7 +10,7 @@ namespace BigDBSorting
             string path = @"..\..\..\Databases\";
             DateTime tt0 = DateTime.Now;
             BigPolar bp = new BigPolar(path);
-            bp.Load2(10000000);
+            bp.Load2(Int32.MaxValue / 32);
             Console.WriteLine("duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
             //bp.Index();
             Console.WriteLine("duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
