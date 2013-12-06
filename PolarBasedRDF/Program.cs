805d99b7d8ec9804da746c7325028c3621f7f14d
Sergey303 <s-agittariu-s@mail.ru>
2013-12-06 20:43:29 +0700

    add RDFTripletsByPolarEngine

---
+++ PolarBasedRDF/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Xml.Linq;
+using PolarBasedEngine;
+
+namespace PolarBasedRDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"..\..\..\Databases\";
+            Console.WriteLine("Start");
+            PolarBasedRdfGraph graph = new PolarBasedRdfGraph(path);
+            bool toload = false;
+            if (toload)
+            {
+                XElement db = XElement.Load(path + "0001.xml");
+                graph.StartFillDb();
+                graph.Load(db.Elements());
+                graph.FinishFillDb();
+            }
+
+            var query = graph.SearchByName("марчук");
+            foreach (XElement rec in query)
+            {
+                Console.WriteLine(rec.ToString());
+            }
+        }
+    }
+}
---
+++ PolarBasedRDF/RDFTripletsByPolarEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PolarDB;
+
+namespace PolarBasedRDF
+{
+    class RDFTripletsByPolarEngine
+    {
+        private PType ptDirects = new PTypeSequence(new PTypeRecord(
+            new NamedType("s", new PType(PTypeEnumeration.sstring)),
+            new NamedType("p", new PType(PTypeEnumeration.sstring)),
+            new NamedType("o", new PType(PTypeEnumeration.sstring))
+            ));
+
+        private PType ptData = new PTypeSequence(new PTypeRecord(
+            new NamedType("s", new PType(PTypeEnumeration.sstring)),
+            new NamedType("p", new PType(PTypeEnumeration.sstring)),
+            new NamedType("d", new PType(PTypeEnumeration.sstring)),
+            new NamedType("l", new PType(PTypeEnumeration.sstring))
+            ));
+
+        private PaCell directCell, dataCell;
+        private string directCellPath, dataCellPath;
+        public RDFTripletsByPolarEngine(string path)
+        {
+            directCell = new PaCell(ptDirects, directCellPath = path + "rdf.direct.pac");
+            dataCell = new PaCell(ptData, dataCellPath = path + "rdf.data.pac");
+        }
+    }
+}
