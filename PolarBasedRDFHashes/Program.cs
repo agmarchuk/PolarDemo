e05f152b2b04dd1be6b50d7ac3e75066de198c64
Sergey303 <s-agittariu-s@mail.ru>
2013-12-15 10:57:56 +0700

    triplets of hashes

---
+++ PolarBasedRDFHashes/Program.cs
@@ -0,0 +1,106 @@
+﻿
+using System;
+using System.IO;
+using System.Linq;
+using PolarDB;
+
+namespace PolarBasedRDF
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string path = @"..\..\..\Databases\";
+            Console.WriteLine("Start");
+            RDFTripletsByPolarEngine graph = new RDFTripletsByPolarEngine(new DirectoryInfo(path));
+            foreach (var count in new[] { 3 * 1000 * 1000 })//3 * 1000 * 1000, 
+            {
+                System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+                using (StreamWriter log = new StreamWriter("../../log.txt", true))
+                    log.WriteLine("start " + count);
+                bool toload = true;
+                if (toload)
+                {
+                    var db = "F:\\freebase-rdf-2013-02-10-00-00.nt2";
+                    //string db = path + "0001.xml";
+                    watch.Restart();
+                    graph.Load(count, db);
+                    watch.Stop();
+                    using (StreamWriter log = new StreamWriter("../../log.txt", true))
+                        log.WriteLine("load db " + watch.Elapsed.Ticks);
+                    
+                    watch.Restart();
+                    graph.LoadIndexes();
+                    watch.Stop();
+                    using (StreamWriter log = new StreamWriter("../../log.txt", true))
+                        log.WriteLine("bufferBYtes= max " + "load indexes " + watch.Elapsed.Ticks.ToString());
+                   
+                }
+               
+
+
+                string[] ids = new[]
+                {
+                    "ns:m.0102c1j",
+                    "ns:m.03cr17b",
+                    "ns:m.03d89qc",
+                    "ns:m.03wtp5x",
+                    "ns:m.03f10_0",
+                    "ns:m.03c0_rb",
+                    "ns:m.03c0_r6",
+                    "ns:m.03y79mn",
+                    "ns:m.051klq",
+                    "ns:m.0dfzyd8",
+                    "ns:m.051b_wb",
+                    "ns:m.0hht9xj",
+                    "ns:m.03cr176",
+                    "ns:m.03d8bgv",
+                    "ns:m.0bmcz6s",
+                    "ns:m.03y79nh",
+                    "ns:m.0gfs6fw",
+                    "ns:m.0hht9xn",
+                    "ns:m.051kly",
+                    "ns:m.0bjbqzq",
+                    "ns:m.03d8bgz",
+                    "ns:m.03cr17b",
+                    "ns:m.03wt5px",
+                    "ns:m.0bmcz6s",
+                    "ns:m.06f9t4",
+                    "ns:m.0bjh9f"
+                    //    "svet_100616111408_10844",
+                    //    "pavl_100531115859_2020",
+                    //    "piu_200809051791",
+                    //    "pavl_100531115859_6952",
+                    //    "svet_100616111408_10864",
+                    //    "w20090506_svetlana_5727",
+                    //    "piu_200809051742",
+                    //    "p0013313",
+                    //    "p0011098",
+                    //    "svet_100616111408_14354"
+
+                };
+                //var query = graph.SearchByName("марчук");
+                //foreach (XElement rec in query) 
+                //{
+                //    Console.WriteLine(rec.ToString());
+                //}
+                watch.Restart();
+                var item = graph.GetItemByIdBasic(ids.First(), true);
+                watch.Stop();
+                //Console.WriteLine(item.ToString());
+                //Console.WriteLine(watch.ElapsedTicks);
+                using (StreamWriter log = new StreamWriter("../../log.txt", true))
+                    log.WriteLine(item.ToString() + watch.Elapsed.Ticks);
+
+                watch.Restart();
+                foreach (string id in ids)
+                {
+                    graph.GetItemByIdBasic(id, true);
+                }
+                watch.Stop();
+                using (StreamWriter log = new StreamWriter("../../log.txt", true))
+                    log.WriteLine("many ids get time = " + watch.Elapsed.Ticks);
+            }
+        }
+    }
+}
---
+++ PolarBasedRDFHashes/RDFTripletsByPolarEngine.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using PolarBasedEngine;
+using PolarDB;
+
+namespace PolarBasedRDF
+{
+    internal class RDFTripletsByPolarEngine
+    {
+        private readonly PType ptDirects = new PTypeSequence(new PTypeRecord(
+            new NamedType("s", new PType(PTypeEnumeration.integer)),
+            new NamedType("p", new PType(PTypeEnumeration.integer)),
+            new NamedType("o", new PType(PTypeEnumeration.integer))
+            ));
+
+        private readonly PType ptData = new PTypeSequence(new PTypeRecord(
+            new NamedType("s", new PType(PTypeEnumeration.integer)),
+            new NamedType("p", new PType(PTypeEnumeration.integer)),
+            new NamedType("d", new PType(PTypeEnumeration.sstring)),
+            new NamedType("l", new PType(PTypeEnumeration.sstring))
+            ));
+        private  readonly  PType ptIds=new PTypeSequence(new PTypeRecord(
+            new NamedType("hash of id", new PType(PTypeEnumeration.integer)),
+            new NamedType("id", new PType(PTypeEnumeration.sstring))));
+
+        private readonly PType ptPred = new PTypeSequence(new PTypeRecord(
+            new NamedType("hash of predicate", new PType(PTypeEnumeration.integer)),
+            new NamedType("predicate", new PType(PTypeEnumeration.sstring))));
+
+        private PaCell directCell, dataCell, predDataCell, predObjell, idCell;
+        private readonly string directCellPath, dataCellPath, predDataCellPath, predObjellPath, idCellPath;
+        private FixedIndex<int> sDirectIndex, oIndex, sDataIndex, predicateDataIndex, predicateObjIndex, idIndex;
+        private  FixedIndex<SubjPred<int>> spDirectIndex, opIndex, spDataIndex;
+
+        public RDFTripletsByPolarEngine(DirectoryInfo path)
+        {
+            if (!path.Exists) path.Create();
+            directCell = new PaCell(ptDirects, directCellPath = Path.Combine(path.FullName, "rdf.hashed.direct.pac"),
+                File.Exists(directCellPath));
+            dataCell = new PaCell(ptData, dataCellPath = Path.Combine(path.FullName, "rdf.hashed.data.pac"),
+                File.Exists(dataCellPath));
+            predDataCell = new PaCell(ptPred, predDataCellPath = Path.Combine(path.FullName, "data predicates.pac"),
+                File.Exists(predDataCellPath));
+            predObjell = new PaCell(ptPred, predObjellPath = Path.Combine(path.FullName, "object  predicates.pac"),
+               File.Exists(predObjellPath));
+            idCell = new PaCell(ptIds, idCellPath = Path.Combine(path.FullName, "ids.pac"),
+               File.Exists(idCellPath));
+            if (dataCell.IsEmpty || directCell.IsEmpty) return;
+            CreateIndexes();
+        }
+
+        private void CreateIndexes()
+        {
+            sDataIndex = new FixedIndex<int>("s of data", dataCell.Root, entry => (int) entry.Field(0).Get());
+            sDirectIndex = new FixedIndex<int>("s of direct", directCell.Root, entry => (int)entry.Field(0).Get());
+            oIndex = new FixedIndex<int>("o of direct", directCell.Root, entry => (int)entry.Field(2).Get());
+
+            predicateObjIndex = new FixedIndex<int>("predicate of object", predObjell.Root, entry => (int)entry.Field(0).Get());
+            predicateDataIndex = new FixedIndex<int>("predicate of data", predDataCell.Root, entry => (int)entry.Field(0).Get());
+            idIndex = new FixedIndex<int>("id", idCell.Root, entry => (int)entry.Field(0).Get());
+
+            spDataIndex = new FixedIndex<SubjPred<int>> ("s and p of data", dataCell.Root,
+                entry =>
+                    new SubjPred<int>((int) entry.Field(0).Get(), (int) entry.Field(1).Get()));
+            spDirectIndex =new FixedIndex<SubjPred<int>>("s and p of direct", directCell.Root,
+                entry =>
+                    new SubjPred<int>((int)entry.Field(0).Get(), (int)entry.Field(1).Get()));
+            opIndex = new FixedIndex<SubjPred<int>>("o and p of direct", directCell.Root,
+                entry =>
+                    new SubjPred<int>((int)entry.Field(0).Get(), (int)entry.Field(1).Get()));
+        }
+
+        #region Load
+
+        public void Load(int tripletsCountLimit, params string[] filesPaths)
+        {
+            directCell.Close();
+            dataCell.Close();
+            predDataCell.Close();
+            predObjell.Close();
+            idCell.Close();
+
+            File.Delete(dataCellPath);
+            File.Delete(directCellPath);
+            File.Delete(predDataCellPath);
+            File.Delete(predObjellPath);
+            File.Delete(idCellPath);
+
+            directCell = new PaCell(ptDirects, directCellPath, false);
+            dataCell = new PaCell(ptData, dataCellPath, false);
+            predDataCell = new PaCell(ptPred, predDataCellPath, false);
+            predObjell = new PaCell(ptPred, predObjellPath, false);
+            idCell = new PaCell(ptIds, idCellPath, false);
+
+            var directSerialFlow = (ISerialFlow) directCell;
+            var dataSerialFlow = (ISerialFlow) dataCell;
+            var idSerialFlow = (ISerialFlow)idCell;
+            var predObjSerialFlow = (ISerialFlow)predObjell;
+            var predDataSerialFlow = (ISerialFlow)predDataCell;
+            directSerialFlow.StartSerialFlow();
+            dataSerialFlow.StartSerialFlow();
+            idSerialFlow.StartSerialFlow();
+            predObjSerialFlow.StartSerialFlow();
+            predDataSerialFlow.StartSerialFlow();
+            directSerialFlow.S();
+            dataSerialFlow.S();
+            idSerialFlow.S();
+            predObjSerialFlow.S();
+            predDataSerialFlow.S();
+            var existsId = new ArrayIntMax<bool>();
+            var existsPredicate = new ArrayIntMax<bool>();
+            int i = 0;
+            for (int j = 0; j < filesPaths.Length && i < tripletsCountLimit; j++)
+                i += ReadFile(filesPaths[j], (id, property, value, isObj, lang) =>
+                {
+                    int hashId = id.GetHashCode();
+                    if (!existsId[hashId])
+                    {
+                        idSerialFlow.V(new object[] {hashId, id});
+                        existsId[hashId] = true;
+                    }
+                    int hProperty = property.GetHashCode();
+                    if (isObj)
+                    {
+                        if (!existsPredicate[hProperty])
+                        {
+                            predObjSerialFlow.V(new object[] {hProperty, property});
+                            existsPredicate[hProperty] = true;
+                        }
+                        var objHash = value.GetHashCode();
+                        directSerialFlow.V(new object[] { hashId, hProperty, objHash });
+                        if (!existsId[objHash])
+                        {
+                            existsId[objHash] = true;
+                            idSerialFlow.V(new object[] { objHash, value });
+                        }
+                    }
+                    else
+                    {
+                        if (!existsPredicate[hProperty])
+                        {
+                            predDataSerialFlow.V(new object[] {hProperty, property});
+                            existsPredicate[hProperty] = true;
+                        }
+                        dataSerialFlow.V(new object[] {hashId, hProperty, value, lang ?? ""});
+                    }
+                },
+                    tripletsCountLimit);
+            directSerialFlow.Se();
+            dataSerialFlow.Se();
+            idSerialFlow.Se();
+            predObjSerialFlow.Se();
+            predDataSerialFlow.Se();
+            directSerialFlow.EndSerialFlow();
+            dataSerialFlow.EndSerialFlow();
+            idSerialFlow.EndSerialFlow();
+            predObjSerialFlow.EndSerialFlow();
+            predDataSerialFlow.EndSerialFlow();
+        }
+
+        internal void LoadIndexes()
+        {
+            if (sDataIndex != null)
+            {
+                sDataIndex.Close();
+                sDirectIndex.Close();
+                oIndex.Close();
+                spDataIndex.Close();
+                spDirectIndex.Close();
+                opIndex.Close();
+                predicateDataIndex.Close();
+                predicateObjIndex.Close();
+                idIndex.Close();
+            }
+            CreateIndexes();
+            sDataIndex.Load(null);
+            sDirectIndex.Load(null);
+            oIndex.Load(null);
+            var subjPredComparer = new SubjPredCoparer<int>();
+            spDataIndex.Load(subjPredComparer);
+            spDirectIndex.Load(subjPredComparer);
+            opIndex.Load(subjPredComparer);
+
+            predicateDataIndex.Load(null);
+            predicateObjIndex.Load(null);
+            idIndex.Load(null);
+        }
+
+        internal delegate void QuadAction(string id, string property,
+            string value, bool isObj = true, string lang = null);
+
+        internal static int ReadFile(string filePath, QuadAction quadAction, int tripletsCountLimit)
+        {
+            var extension = Path.GetExtension(filePath);
+            if (extension == null || !File.Exists(filePath)) return 0;
+            extension = extension.ToLower();
+            if (extension == ".xml")
+                return ReadXML2Quad(filePath, quadAction, tripletsCountLimit);
+            else if (extension == ".nt2")
+                return ReadTSV(filePath, quadAction, tripletsCountLimit);
+            return 0;
+        }
+
+        private static readonly Regex NsRegex = new Regex(@"^@prefix\s+(\w+):\s+<(.+)>\.$",
+            RegexOptions.Compiled | RegexOptions.Singleline);
+
+        private static readonly Regex TripletsRegex = new Regex("^(\\S+)\\s+(\\S+)\\s+(\"(.+)\"(@(\\S*))?|(.+))\\.$",
+            RegexOptions.Compiled | RegexOptions.Singleline);
+
+        /// <summary>
+        /// TODO ns
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="quadAction"></param>
+        /// <param name="tripletsCountLimit"></param>
+        private static int ReadTSV(string filePath, QuadAction quadAction, int tripletsCountLimit)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                Match nsReg;
+                while ((nsReg = NsRegex.Match(reader.ReadLine())).Success)
+                {
+                    // nsReg.Groups[1]
+                    //nsReg.Groups[2]
+                }
+                int tryCount = 0;
+                const int tryLinesCountMax = 10;
+                string readLine = string.Empty;
+                int i;
+                for (i = 0; i < tripletsCountLimit && !reader.EndOfStream; i++, readLine = string.Empty, tryCount = 0)
+                    while (tryCount < tryLinesCountMax && !String2Quard(quadAction, readLine += reader.ReadLine()))
+                        tryCount++;
+                return i + 1;
+            }
+        }
+
+        private static bool String2Quard(QuadAction quadAction, string readLine)
+        {
+            Match lineMatch;
+            if (!(lineMatch = TripletsRegex.Match(readLine)).Success) return false;
+            var dMatch = lineMatch.Groups[4];
+            if (dMatch.Success)
+                quadAction(lineMatch.Groups[1].Value, lineMatch.Groups[2].Value, dMatch.Value, false,
+                    lineMatch.Groups[6].Value);
+            else
+                quadAction(lineMatch.Groups[1].Value, lineMatch.Groups[2].Value, lineMatch.Groups[7].Value);
+            return true;
+        }
+
+        #region Read XML
+
+        public static string LangAttributeName = "xml:lang",
+            RDFAbout = "rdf:about",
+            RDFResource = "rdf:resource",
+            NS = "http://fogid.net/o/";
+
+
+        /// <summary>
+        /// TODO tripletsCountLimit
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="quadAction"></param>
+        /// <param name="tripletsCountLimit"></param>
+        private static int ReadXML2Quad(string url, QuadAction quadAction, int tripletsCountLimit)
+        {
+            string id = string.Empty;
+            int i = 0;
+            using (var xml = new XmlTextReader(url))
+                while (i < tripletsCountLimit && xml.Read())
+                    if (xml.IsStartElement())
+                        if (xml.Depth == 1 && (id = xml[RDFAbout]) != null)
+                        {
+                            quadAction(id, ONames.rdftypestring, NS + xml.Name);
+                            i++;
+                        }
+                        else if (xml.Depth == 2 && id != null)
+                        {
+                            string resource = xml[RDFResource];
+                            bool isObj = (resource) != null;
+                            quadAction(id, NS + xml.Name,
+                                isObj: isObj,
+                                lang: isObj ? null : xml[LangAttributeName],
+                                value: isObj ? resource : xml.ReadString());
+                            i++;
+                        }
+            return i + 1;
+        }
+
+        #endregion
+
+        #endregion
+
+        public string GetItemHashesInterpret(string id)
+        {
+            return
+                sDirectIndex.GetAllByKey(id.GetHashCode())
+                    .Select(spo => spo.Type.Interpret(spo.Get()))
+                    .Concat(
+                        oIndex.GetAllByKey(id.GetHashCode())
+                            .Select(spo => spo.Type.Interpret(spo.Get()))
+                            .Concat(
+                                sDataIndex.GetAllByKey(id.GetHashCode())
+                                    .Select(spo => spo.Type.Interpret(spo.Get()))))
+                    .Aggregate((all, one) => all + one);
+        }
+
+        public XElement GetItemByIdBasic(string id, bool addinverse)
+        {
+            int hid = id.GetHashCode();
+            var type = spDirectIndex.GetFirstByKey(new SubjPred<int>(hid, ONames.rdftypestring.GetHashCode()));
+            XElement res = new XElement("record", new XAttribute("id", id), type.offset == long.MinValue ? null : new XAttribute("type", idIndex.GetFirstByKey(((object[])type.Get())[2].GetHashCode()).Field(1).Get()),
+                sDataIndex.GetAllByKey(hid).Select(entry => entry.Get()).Cast<object[]>().Select(v3 =>
+                    new XElement("field", new XAttribute("prop", predicateDataIndex.GetFirstByKey((int)v3[1]).Field(1).Get()),
+                        string.IsNullOrEmpty((string) v3[3]) ? null : new XAttribute(ONames.xmllang, v3[3]),
+                        v3[2])),
+                 sDirectIndex.GetAllByKey(hid).Select(entry => entry.Get()).Cast<object[]>().Select(v2 =>
+                    new XElement("direct", new XAttribute("prop", predicateObjIndex.GetFirstByKey((int)v2[1]).Field(1).Get()),
+                        new XElement("record", new XAttribute("id", idIndex.GetFirstByKey((int)v2[2]).Field(1).Get())))),
+                null);
+            // Обратные ссылки
+            if (addinverse)
+            {
+                var query = oIndex.GetAllByKey(hid);
+                string predicate = null;
+                XElement inverse = null;
+                foreach (PaEntry en in query)
+                {
+                    var rec = (object[]) en.Get();
+                    string pred = (string) predicateObjIndex.GetFirstByKey((int)rec[1]).Field(1).Get();
+                    if (pred != predicate)
+                    {
+                        res.Add(inverse);
+                        inverse = new XElement("inverse", new XAttribute("prop", pred));
+                        predicate = pred;
+                    }
+                    string idd = (string) idIndex.GetFirstByKey((int)rec[0]).Field(1).Get();
+                    inverse.Add(new XElement("record", new XAttribute("id", idd)));
+                }
+                res.Add(inverse);
+            }
+            return res;
+        }
+       
+    }
+
+    class ArrayIntMax<T>
+    {
+       const int MaxArrayIndex = 2146435071;
+       const int LastCount = Int32.MaxValue - MaxArrayIndex; 
+        private readonly T[] negativeLast, negativeFirst, positiveFirst, positiveLast;
+
+
+        public ArrayIntMax()
+        {
+            negativeLast = new T[LastCount];
+            negativeFirst = new T[MaxArrayIndex];
+            positiveFirst = new T[MaxArrayIndex];
+            positiveLast = new T[LastCount];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return index >= 0
+                    ? (index >= MaxArrayIndex ? positiveLast[index - MaxArrayIndex] : positiveFirst[index])
+                    : (index <= -MaxArrayIndex ? negativeLast[-index - MaxArrayIndex] : negativeFirst[-index]);
+            }
+            set
+            {
+                if (index > 0)
+                    if (index >= MaxArrayIndex) positiveLast[index - MaxArrayIndex] = value;
+                    else positiveFirst[index]=value;
+                else if (index <= -MaxArrayIndex) negativeLast[-index - MaxArrayIndex] = value;
+                else negativeFirst[-index]=value;
+            }
+        }
+    }
+}
