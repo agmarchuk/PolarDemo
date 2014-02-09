185c554aac682bbf2999879422623f9c731ea6d5
Alexander Marchuk <alexander.g.marchuk@gmail.com>
2014-02-09 22:06:23 +0700

    Добавлен проект TrueRdfViewer.csproj

---
+++ TrueRdfViewer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueRdfViewer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] ids = new string []
+            {
+                //"svet_100616111408_10844",
+                //"pavl_100531115859_2020",
+                //"piu_200809051791",
+                //"pavl_100531115859_6952",
+                //"svet_100616111408_10864",
+                //"w20090506_svetlana_5727",
+                //"piu_200809051742",
+                //"p0013313",
+                //"p0011098",
+                //"svet_100616111408_14354"
+
+                "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature13",
+                "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature4",
+                "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature8",
+                "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature11",
+                "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature3",
+                "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature19",
+            };
+
+            DateTime tt0 = DateTime.Now;
+            Console.WriteLine("Start");
+            string path = "../../../Databases/";
+            TripleStore ts = new TripleStore(path);
+            bool toload = true;
+            if (toload)
+            {
+                //ts.LoadXML(path + "0001.xml");
+                //Console.WriteLine("LoadXML ok.");
+                ts.LoadTurtle(@"D:\home\FactographDatabases\dataset\dataset10m.ttl");
+                Console.WriteLine("LoadTurtle ok.");
+                Console.WriteLine("duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
+                return;
+            }
+
+            var qu = ts.GetItem("http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature19");
+            Console.WriteLine("duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
+            //if (qu != null) Console.WriteLine(qu.ToString());
+        
+            tt0 = DateTime.Now;
+            foreach (string id in ids)
+            {
+                var query = 
+                    ts.GetSubjectByObjPred(id, "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productFeature")
+                    .Where(_product => ts.ChkOSubjPredObj(_product, "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productFeature", "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature8"))
+                    .Where(_product => ts.ChkOSubjPredObj(_product, "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductType1"))
+                    .SelectMany(_product => ts.GetDataBySubjPred(_product, "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productPropertyNumeric1"))
+                    ;
+                int cnt = query.Count();
+                Console.WriteLine(cnt);
+                //if (id == "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/ProductFeature19")
+                //foreach (var dd in query)
+                //{
+                //    Console.WriteLine("dd={0}", dd);
+                //}
+            }
+            Console.WriteLine("duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
+        }
+    }
+}
---
+++ TrueRdfViewer/TripleStore.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using PolarDB;
+using sema2012m;
+
+namespace TrueRdfViewer
+{
+    public class TripleStore
+    {
+        private string path;
+        private PType tp_otriple_seq;
+        private PType tp_dtriple_seq;
+        private PaCell otriples;
+        private PaCell dtriples;
+        private FlexIndexView<SubjPredObj> spo_o_index = null;
+        private FlexIndexView<SubjPred> sp_d_index = null;
+        private FlexIndexView<SubjPred> op_o_index = null;
+
+        public TripleStore(string path)
+        {
+            this.path = path;
+            InitTypes();
+            otriples = new PaCell(tp_otriple_seq, path + "otriples.pac", false);
+            dtriples = new PaCell(tp_dtriple_seq, path + "dtriples.pac", false);
+            if (!otriples.IsEmpty)
+            {
+                OpenCreateIndexes();
+            }
+        }
+
+        private void OpenCreateIndexes()
+        {
+            spo_o_index = new FlexIndexView<SubjPredObj>(path + "spo_o_index", otriples.Root,
+                ent => new SubjPredObj() { subj = (string)ent.Field(0).Get(), pred = (string)ent.Field(1).Get(), obj = (string)ent.Field(2).Get() });
+            sp_d_index = new FlexIndexView<SubjPred>(path + "subject_d_index", dtriples.Root,
+                ent => new SubjPred() { subj = (string)ent.Field(0).Get(), pred = (string)ent.Field(1).Get() });
+            op_o_index = new FlexIndexView<SubjPred>(path + "obj_o_index", otriples.Root,
+                ent => new SubjPred() { subj = (string)ent.Field(2).Get(), pred = (string)ent.Field(1).Get() });
+        }
+
+        public void LoadTurtle(string filepath)
+        {
+            otriples.Clear();
+            otriples.Fill(new object[0]);
+            dtriples.Clear();
+            dtriples.Fill(new object[0]);
+            int i = 0;
+            foreach (var triple in Turtle.LoadGraph(filepath))
+            {
+                if (i % 10000 == 0) Console.Write("{0} ", i);
+                i++;
+                if (triple is OTriple)
+                {
+                    var tr = (OTriple)triple;
+                    //Console.WriteLine(tr.obj);
+                    otriples.Root.AppendElement(new object[] { tr.sublect, tr.predicate, tr.obj });
+                }
+                else
+                {
+                    var tr = (DTriple)triple;
+                    //Console.WriteLine("{0} {1}", tr.data.vid, tr.data.value);
+                    dtriples.Root.AppendElement(new object[] { tr.sublect, tr.predicate, 
+                        new object[] { 2, new object[] { tr.data.value, "en" } } });
+                }
+            }
+            Console.WriteLine();
+            otriples.Flush();
+            dtriples.Flush();
+            // Индексирование
+            if (spo_o_index == null)
+            {
+                OpenCreateIndexes();
+            }
+            spo_o_index.Load(null);
+            sp_d_index.Load(null);
+            op_o_index.Load(null);
+        }
+        public void LoadXML(string filepath)
+        {
+            otriples.Clear();
+            otriples.Fill(new object[0]);
+            dtriples.Clear();
+            dtriples.Fill(new object[0]);
+            XElement db = XElement.Load(filepath);
+            foreach (XElement elem in db.Elements())
+            {
+                var id_att = elem.Attribute(ONames.rdfabout);
+                if (id_att == null) continue;
+                string id = id_att.Value;
+                string typ = elem.Name.NamespaceName + elem.Name.LocalName;
+                otriples.Root.AppendElement(
+                    new object[] { id, ONames.rdfnsstring + "type", typ } );
+                foreach (XElement el in elem.Elements())
+                {
+                    string prop = el.Name.NamespaceName + el.Name.LocalName;
+                    var resource_att = el.Attribute(ONames.rdfresource);
+                    if (resource_att != null)
+                    { // Объектная ссылка
+                        otriples.Root.AppendElement(
+                            new object[] { id, prop, resource_att.Value } );
+                    }
+                    else
+                    { // Поле данных
+                        var lang_att = el.Attribute(ONames.xmllang);
+                        string lang = lang_att == null ? "" : lang_att.Value;
+                        dtriples.Root.AppendElement(
+                            new object[] { id, prop, 
+                                new object[] { 2, new object[] { el.Value, lang } } } );
+                    }
+                }
+            }
+            otriples.Flush();
+            dtriples.Flush();
+            // Индексирование
+            if (!otriples.IsEmpty)
+            {
+                OpenCreateIndexes();
+            }
+            spo_o_index.Load(null);
+            sp_d_index.Load(null);
+            op_o_index.Load(null);
+        }
+
+        public IEnumerable<string> GetSubjectByObjPred(string obj, string pred)
+        {
+            return op_o_index.GetAll(ent => 
+            {
+                string ob = (string)ent.Field(2).Get();
+                int cmp = ob.CompareTo(obj);
+                if (cmp != 0) return cmp;
+                string pr = (string)ent.Field(1).Get();
+                return pr.CompareTo(pred);
+            })
+                .Select(en => (string)en.Field(0).Get());
+        }
+        public IEnumerable<string> GetObjBySubjPred(string subj, string pred)
+        {
+            return spo_o_index.GetAll(ent =>
+            {
+                string su = (string)ent.Field(0).Get();
+                int cmp = su.CompareTo(subj);
+                if (cmp != 0) return cmp;
+                string pr = (string)ent.Field(1).Get();
+                return pr.CompareTo(pred);
+            })
+                .Select(en => (string)en.Field(2).Get());
+        }
+        public IEnumerable<Literal> GetDataBySubjPred(string subj, string pred)
+        {
+            return sp_d_index.GetAll(ent =>
+            {
+                string su = (string)ent.Field(0).Get();
+                int cmp = su.CompareTo(subj);
+                if (cmp != 0) return cmp;
+                string pr = (string)ent.Field(1).Get();
+                return pr.CompareTo(pred);
+            })
+                .Select(en => 
+                {
+                    object[] uni = (object[])en.Field(2).Get();
+                    Literal lit = new Literal();
+                    int vid = (int)uni[0];
+                    if (vid == 1) { lit.vid = LiteralVidEnumeration.integer; lit.value = (int)uni[1]; }
+                    if (vid == 3) { lit.vid = LiteralVidEnumeration.date; lit.value = (long)uni[1]; }
+                    else if (vid == 2)
+                    {
+                        lit.vid = LiteralVidEnumeration.text;
+                        object[] txt = (object[])uni[1];
+                        lit.value = new Text() { s = (string)txt[0], l = (string)txt[1] };
+                    }
+                    return lit;
+                });
+        }
+        public bool ChkOSubjPredObj(string subj, string pred, string obj)
+        {
+            return !spo_o_index.GetFirst(ent =>
+            {
+                string su = (string)ent.Field(0).Get();
+                int cmp = su.CompareTo(subj);
+                if (cmp != 0) return cmp;
+                string pr = (string)ent.Field(1).Get();
+                cmp = pr.CompareTo(pred);
+                if (cmp != 0) return cmp;
+                string ob = (string)ent.Field(2).Get();
+                return ob.CompareTo(obj);
+            }).IsEmpty;
+        }
+        public XElement GetItem(string subject)
+        {
+            if (otriples.Root.Count() == 0) return null;
+            if (dtriples.Root.Count() == 0) return null;
+            XElement res = new XElement("record", new XAttribute("id", subject));
+            //PaEntry dent = dtriples.Root.Element(0);
+            //foreach (var ent in subject_d_index.GetAllByKey(subject))
+            foreach (var ent in sp_d_index.GetAll(en => ((string)en.Field(0).Get()).CompareTo(subject)))
+            {
+                object[] tr = (object[])ent.Get();
+                string predicate = (string)tr[1];
+                object[] literal = (object[])tr[2];
+                res.Add(new XElement("field", new XAttribute("prop", predicate),
+                    ((object[])literal[1])[0]));
+            }
+            string type = null;
+            //PaEntry oent = otriples.Root.Element(0);
+            //foreach (var ent in subject_o_index.GetAllByKey(subject))
+            foreach (var ent in spo_o_index.GetAll(en => ((string)en.Field(0).Get()).CompareTo(subject)))
+            {
+                object[] tr = (object[])ent.Get();
+                string predicate = (string)tr[1];
+                string obj = (string)tr[2];
+                if (predicate == ONames.rdftypestring)
+                {
+                    type = obj;
+                }
+                else
+                {
+                    res.Add(new XElement("direct", new XAttribute("prop", predicate),
+                        new XElement("record", new XAttribute("id", obj))));
+                }
+            }
+            if (type != null) res.Add(new XAttribute("type", type));
+            // Обратные ссылки
+            //foreach (var ent in obj_o_index.GetAllByKey(subject))
+            foreach (var ent in op_o_index.GetAll(en => ((string)en.Field(2).Get()).CompareTo(subject)))
+            {
+                object[] tr = (object[])ent.Get();
+                string subj = (string)tr[0];
+                string predicate = (string)tr[1];
+                res.Add(new XElement("inverse", new XAttribute("prop", predicate),
+                    subj));
+            }
+
+            return res;
+        }
+
+        private void InitTypes()
+        {
+            PType tp_rliteral = new PTypeUnion(
+                new NamedType("void", new PType(PTypeEnumeration.none)),
+                new NamedType("integer", new PType(PTypeEnumeration.integer)),
+                new NamedType("string", new PTypeRecord(
+                    new NamedType("s", new PType(PTypeEnumeration.sstring)),
+                    new NamedType("l", new PType(PTypeEnumeration.sstring)))),
+                new NamedType("date", new PType(PTypeEnumeration.longinteger)));
+            tp_otriple_seq = new PTypeSequence(new PTypeRecord(
+                    new NamedType("subject", new PType(PTypeEnumeration.sstring)),
+                    new NamedType("predicate", new PType(PTypeEnumeration.sstring)),
+                    new NamedType("object", new PType(PTypeEnumeration.sstring))));
+            tp_dtriple_seq = new PTypeSequence(new PTypeRecord(
+                    new NamedType("subject", new PType(PTypeEnumeration.sstring)),
+                    new NamedType("predicate", new PType(PTypeEnumeration.sstring)),
+                    new NamedType("data", tp_rliteral)));
+        }
+    }
+}
