47daa7eb8f36b5402d8cffad2c52e677c221f580
sergey303 <luxveritatis@live.ru>
2014-03-03 18:42:27 +0700

    настроил на x64

--- TrueRdfViewer/EntitiesDiapasons.cs
+++ TrueRdfViewer/EntitiesDiapasons.cs
@@ -1,43 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using PolarDB;
 
 namespace TrueRdfViewer
 {
-    class EntitiesDiapasons
+    class EntitiesMemoryDiapasons
     {
-        public EntitiesDiapasons(Entities entities)
+        public EntitiesMemoryDiapasons(Entities entities)
         {
-            _entities = entities;
+            this.entities = entities;
+        
         }
-
-
-        private static short bytesPerHash = 22, hashShift=(short)(32-bytesPerHash);
-        int Hash(int code)
-        {
-            return code >> hashShift;
-        }
-
-        DiapasonShot[] hashIndex=new DiapasonShot[(int)Math.Pow(2,bytesPerHash)];
-        private readonly Entities _entities;
-
+                                      
+        private readonly Entities entities;
+        private readonly Dictionary<int, Diapason> diapasonsTable;
         public PaEntry GetDiapason(int idCode)
         {
-            var hashDiapason = hashIndex[Hash(idCode)];
-            if (hashDiapason.Numb == 0) return PaEntry.Empty;
-            return _entities.EntitiesTable.Root.BinarySearchFirst(hashDiapason.Start, Convert.ToInt64(hashDiapason.Numb),
-                entry => (int) entry.Field(0).Get() - idCode);
-        }
-    }
-
-    struct DiapasonShot
-    {
-        public int Start;
-        public short Numb;
-
-        public DiapasonShot(int start, short numb)
-        {
-            Start = start;
-            Numb = numb;
+            
         }
     }
 }
