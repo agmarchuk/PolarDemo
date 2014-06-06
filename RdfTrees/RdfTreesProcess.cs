0f1d183938dd072c72c96a3543199821aea2367d
sergey303 <s@s.s>
2014-06-06 17:07:44 +0700

    sPO SPo sPD

--- RdfTrees/RdfTreesProcess.cs
+++ RdfTrees/RdfTreesProcess.cs
@@ -58,31 +58,51 @@ namespace RdfTrees
         }
 
 
-        //public override IEnumerable<KeyValuePair<int, int>> GetObjBySubj(int subj)
-        //{
-        //    if (countCalls++ >= callsMaxCount) return Enumerable.Empty<KeyValuePair<Int32, Int32>>();
-        //    var res = base.GetObjBySubj(subj);
-        //    if (isWrite)
-        //        x.Add(new XElement("sPO", new XAttribute("subj", TripleInt.DecodeEntities(subj)), new XAttribute("res", string.Join(" ", res.Select(literal => TripleInt.DecodeEntities(literal.Key) + " " + TripleInt.DecodePredicates(literal.Value))))));
-        //    return res;
-        //}
-        //public override IEnumerable<KeyValuePair<Literal, int>> GetDataBySubj(int subj)
-        //{
-        //    if (countCalls++ >= callsMaxCount) return Enumerable.Empty<KeyValuePair<Literal, Int32>>();
-        //    var res = base.GetDataBySubj(subj);
-        //    if (isWrite)
-        //        x.Add(new XElement("sPD", new XAttribute("subj", TripleInt.DecodeEntities(subj)),
-        //            new XAttribute("res", string.Join(" ", res.Select(literal => literal.Key + " " + TripleInt.DecodePredicates(literal.Value))))));
-        //    return res;
-        //}
-        //public override IEnumerable<KeyValuePair<int, int>> GetSubjectByObj(int obj)
-        //{
-        //    if (countCalls++ >= callsMaxCount) return Enumerable.Empty<KeyValuePair<int, Int32>>();
-        //    var res = base.GetSubjectByObj(obj);
-        //    if (isWrite)
-        //        x.Add(new XElement("SPo", new XAttribute("obj", TripleInt.DecodeEntities(obj)),
-        //        new XAttribute("res", string.Join(" ", res.Select(literal => TripleInt.DecodeEntities(literal.Key) + " " + TripleInt.DecodePredicates(literal.Value))))));
-        //    return res;
-        //}
+        /// <summary>
+        /// keys entities
+        /// </summary>
+        /// <param name="subj"></param>
+        /// <returns></returns>
+        public override IEnumerable<KeyValuePair<int, int>> GetObjBySubj(int subj)
+        {
+            var rec_ent = this.entitiesTree.Root.BinarySearchFirst(ent => ((int)ent.Field(0).Get()).CompareTo(subj));
+            if (rec_ent.IsEmpty) return Enumerable.Empty<KeyValuePair<int, int>>();
+            object[] pairs = (object[])rec_ent.Field(2).Get();
+            var query = pairs.Cast<object[]>().Select(pair => new KeyValuePair<int, int>((int)pair[1], (int) pair[0]));
+            return query;
+        }
+     
+        public override IEnumerable<KeyValuePair<Literal, int>> GetDataBySubj(int subj)
+        {
+            var rec_ent = this.entitiesTree.Root.BinarySearchFirst(ent => ((int)ent.Field(0).Get()).CompareTo(subj));
+            if (rec_ent.IsEmpty) return Enumerable.Empty<KeyValuePair<Literal, int>>();
+            object[] pairs = (object[])rec_ent.Field(1).Get();
+            PaEntry dtriple_entry = dtriples.Root.Element(0);
+            var query = pairs.Cast<object[]>()
+                .Select(pair =>
+                {
+                    dtriple_entry.offset = (long)pair[1];
+                    var literal_obj = dtriple_entry.Field(2).Get();
+                    return new KeyValuePair<Literal, int>(Literal.ToLiteral((object[])literal_obj), (int) pair[0]); 
+                });
+            return query;
+        }
+        /// <summary>
+        /// keys entities
+        /// </summary>
+        /// <param name="subj"></param>
+        /// <returns></returns>
+        public override IEnumerable<KeyValuePair<int, int>> GetSubjectByObj(int obj)
+        {
+            var rec_ent = this.entitiesTree.Root.BinarySearchFirst(ent => ((int)ent.Field(0).Get()).CompareTo(obj));
+            if (rec_ent.IsEmpty) return Enumerable.Empty<KeyValuePair<int, int>>();
+            List<KeyValuePair<int,int>> subjs = new List<KeyValuePair<int, int>>();
+            foreach (var pred_subjseq in rec_ent.Field(3).Elements())
+            {
+                var p = (int)pred_subjseq.Field(0).Get();
+                subjs.AddRange(from int s in (object[]) pred_subjseq.Field(1).Get() select new KeyValuePair<int, int>(s, p));
+            }
+            return subjs;
+        }
     }
 }
