4636ccb962af66b25adf706269caf8dae2a4b887
Alexander Marchuk <alexander.g.marchuk@gmail.com>
2014-10-08 12:43:50 +0700

    Какие-то реорганизации

---
+++ PObjectives/Collection2.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using PolarDB;
+
+namespace PObjectives
+{
+    public class Collection2
+    {
+        private string collectionname;
+        public string Name { get { return collectionname; } }
+        private XElement frecord;
+        public XElement FRecord { get { return frecord; } }
+        private PType eType;
+        public PType CollectionElementType { get { return eType; } }
+        private Database2 inDatabase;
+        internal Database2 InDatabase { get { return inDatabase; } }
+        private PType eeType; // extended element type
+        private PaCell cell;
+        private FlexIndex2<int> key_index;
+        private List<IIndex> indexes = new List<IIndex>();
+        //private int keyNew = 0;
+        //private int counter = -1;
+        internal Collection2(string cname, XElement schema, Database2 inDatabase)
+        {
+            collectionname = cname;
+            this.inDatabase = inDatabase;
+            this.frecord = schema.Elements("record").First(re => re.Attribute("type").Value == cname);
+            string ftype = cname;
+            // Это будет коллекция записей, сначала выявим тип записи для коллекции
+            NamedType[] nt_arr = frecord.Elements()
+                .Where(el => el.Name == "field" || el.Name == "direct")
+                .Select(el =>
+                {
+                    PType tpe = null;
+                    if (el.Name == "direct")
+                    {
+                        tpe = new PType(PTypeEnumeration.integer);
+                    }
+                    else if (el.Name == "field")
+                    {
+                        string el_type = el.Attribute("datatype").Value;
+                        if (el_type == "string") tpe = new PType(PTypeEnumeration.sstring);
+                        else if (el_type == "int") tpe = new PType(PTypeEnumeration.integer);
+                    }
+                    return new NamedType(el.Attribute("prop").Value, tpe);
+                })
+                .ToArray();
+            this.eType = new PTypeRecord(nt_arr);
+            
+            this.eeType = new PTypeRecord(
+                new NamedType("deleted", new PType(PTypeEnumeration.boolean)),
+                new NamedType("key", new PType(PTypeEnumeration.integer)),
+                new NamedType("element", eType));
+            string path = inDatabase.Path;
+            cell = new PaCell(new PTypeSequence(eeType), path + cname + ".pac", false);
+            if (cell.IsEmpty) cell.Fill(new object[0]);
+            key_index = new FlexIndex2<int>(path + cname + "_id_i", cell.Root, en => (int)en.Field(1).Get(), null);
+            // Другие индексы
+            foreach (var pair in frecord.Elements().Select((el, ind) => new {el=el, ind=ind}))
+            {
+                if (pair.el.Name != "direct") continue;
+                string column = pair.el.Attribute("prop").Value;
+                string totype = pair.el.Element("record").Attribute("type").Value;
+                string name_combination = ftype + "(" + column + ")" + totype;
+                FlexIndex2<int> index = new FlexIndex2<int>(path + name_combination + ".pac", cell.Root,
+                    (PaEntry ent) => (int)ent.Field(2).Field(pair.ind).Get(), null);
+                // Запишем в местный список
+                indexes.Add(index);
+                // Запишем в общий список
+                inDatabase.external_indexes.Add(new IndexContext() { type = ftype, prop = column, totype = totype, index = index });
+
+            }
+        }
+        public void Clear()
+        {
+            cell.Clear();
+            cell.Fill(new object[0]);
+            key_index.Load();
+            foreach (var index in indexes) index.Load();
+        }
+        // Быстрое добавление элемента при загрузке. В конце серии, требуется Flush(). 
+        public void AppendElement(int key, object pvalue)
+        {
+            var off = cell.Root.AppendElement(new object[] { false, key, pvalue });
+        }
+        public void Flush()
+        {
+            cell.Flush();
+            // Кроме сброса размера в ячейку, произведем вычисление индекса
+            key_index.Load();
+            foreach (var index in indexes) index.Load();
+        }
+        // "Медленное" добавление элемента, Flush() не требуется
+        public PaEntry AddElement(int key, object pvalue)
+        {
+            var off = cell.Root.AppendElement(new object[] { false, key, pvalue });
+            cell.Flush();
+            PaEntry entry = new PaEntry(eeType, off, cell);
+            key_index.AddEntry(entry);
+            foreach (var index in indexes) index.AddEntry(entry);
+            return entry;
+        }
+        //// Создание нового элемента - (пока) отменено в силу нерешенности вопроса о генерации новых ключей
+        //public Element CreateElement(object pvalue)
+        //{
+        //    var off = cell.Root.AppendElement(new object[] { false, keyNew, pvalue });
+        //    cell.Flush();
+        //    PaEntry entry = new PaEntry(eeType, off, cell);
+        //    key_index.AppendEntry(entry);
+        //    keyNew++;
+        //    return new Element() { entry = entry, inCollection = this };
+        //}
+        public void UpdateElement(Element element, object pvalue)
+        {
+            bool deleted = (bool)element.entry.Field(0).Get();
+            if (deleted) throw new Exception("deleted element can't be updated");
+            int key = (int)element.entry.Field(1).Get();
+            element.entry.Field(0).Set(true);
+            PaEntry entry = AddElement(key, pvalue);
+            element.entry = entry;
+        }
+        public IEnumerable<Element2> Elements()
+        {
+            return cell.Root.Elements()
+                .Where(en => !(bool)en.Field(0).Get())
+                .Select(en => new Element2() { entry = en, inCollection = this });
+        }
+        public IEnumerable<object> ElementValues()
+        {
+            return cell.Root.Elements()
+                .Select<PaEntry, object[]>(en => (object[])en.Get())
+                .Where(ev => !(bool)ev[0])
+                .Select(ev => ev[2]);
+        }
+        public Element2 Element(int key)
+        {
+            PaEntry entry = GetEntryByKey(key);
+            return new Element2() { entry = entry, inCollection = this };
+        }
+        public TElement Element<TElement>(int key) where TElement : Element2, new()
+        {
+            Element2 el = this.Element(key);
+            var res = new TElement() { inCollection = el.inCollection, entry = el.entry };
+            return res;
+        }
+        private Dictionary<int, long> key_entry_dic = new Dictionary<int, long>();
+        internal PaEntry GetEntryByKey(int key)
+        {
+            if (cell.Root.Count() == 0) return PaEntry.Empty;
+            long off;
+            if (key_entry_dic.TryGetValue(key, out off))
+            {
+                PaEntry en = cell.Root.Element(0);
+                en.offset = off;
+                return en;
+            }
+            PaEntry ent = key_index.GetFirstByKey(key);
+            key_entry_dic.Add(key, ent.offset);
+            return ent;
+        }
+    }
+}
---
+++ PObjectives/ProgramConvert.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PObjectives
+{
+    class ProgramConvert
+    {
+        public static void Main2(string[] args)
+        {
+            DateTime tt0 = DateTime.Now;
+            Console.WriteLine("ConvertProg start");
+            string filename = @"C:\home\FactographDatabases\PolarDemo\0001.xml";
+            XElement db = XElement.Load(filename);
+            Console.WriteLine("Load ok. duration={0}", (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
+            //Console.WriteLine(db.Elements().Count()); // 176566
+
+            // Посмотрим сколько имеется классов
+            Dictionary<string, int> enames = new Dictionary<string, int>();
+            foreach (XElement element in db.Elements())
+            {
+                string ename = element.Name.NamespaceName + element.Name.LocalName;
+                int n = -1;
+                if (enames.TryGetValue(ename, out n))
+                { // Есть в словаре
+                    enames[ename] = n + 1;
+                }
+                else
+                { // нет в словаре
+                    enames.Add(ename, 1);
+                }
+            }
+            Console.WriteLine(enames.Count); // 23
+            Console.WriteLine("Load ok. duration={0}", (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
+
+            // Посмотрим сколько в классах элементов
+            foreach (var pair in enames)
+            {
+                Console.WriteLine("{0} {1}", pair.Key, pair.Value);
+            }
+
+            // Сделаем выборку трех классов (три сосны...) person, photo-doc, reflection. Это почти 100 тыс. объектов, т.е. больше половины от всех.
+            string formats_str =
+@"<formats>
+    <record type='http://fogid.net/o/person'>
+        <field prop='http://fogid.net/o/name'/>
+        <field prop='http://fogid.net/o/from-date'/>
+        <field prop='http://fogid.net/o/description'/>
+        <field prop='http://fogid.net/o/to-date'/>
+        <field prop='http://fogid.net/o/sex'/>
+    </record>
+    <record type='http://fogid.net/o/photo-doc'>
+        <field prop='http://fogid.net/o/name'/>
+        <field prop='http://fogid.net/o/from-date'/>
+        <field prop='http://fogid.net/o/description'/>
+    </record>
+    <record type='http://fogid.net/o/reflection'>
+        <field prop='http://fogid.net/o/ground'/>
+        <direct prop='http://fogid.net/o/reflected'/>
+        <direct prop='http://fogid.net/o/in-doc'/>
+    </record>
+</formats>";
+            XElement formats = XElement.Parse(formats_str);
+
+            XElement db1 = new XElement("db");
+            // Поехали...
+            foreach (XElement element in db.Elements())
+            {
+                string ename = element.Name.NamespaceName + element.Name.LocalName;
+                XElement format = formats.Elements().FirstOrDefault(e => e.Attribute("type").Value == ename);
+                if (format == null) continue;
+                string id = element.Attribute("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about").Value;
+                int c = CodeId(id);
+                XElement el = new XElement(element.Name.LocalName, new XAttribute("id", c));
+                bool wrong_element = false;
+                foreach (XElement fel in format.Elements())
+                {
+                    string prop = fel.Attribute("prop").Value;
+                    string pro = prop.Substring(prop.LastIndexOf('/') + 1);
+                    XElement subel = new XElement(pro);
+                    if (fel.Name == "field")
+                    {
+                        XElement f = element.Elements().FirstOrDefault(e => e.Name.NamespaceName + e.Name.LocalName == prop);
+                        if (f != null) subel.Add(f.Value);
+                    }
+                    else if (fel.Name == "direct")
+                    {
+                        XElement d = element.Elements().FirstOrDefault(e => e.Name.NamespaceName + e.Name.LocalName == prop);
+                        if (d != null)
+                        {
+                            XAttribute r = d.Attribute("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource");
+                            if (r != null)
+                            {
+                                subel.Add(new XAttribute("ref", CodeId(r.Value)));
+                            }
+                            else { wrong_element = true; continue; }
+                        }
+                        else { wrong_element = true; continue; }
+                    }
+                    el.Add(subel);
+                }
+                if (!wrong_element) db1.Add(el);
+            }
+            db1.Save(@"C:\home\FactographDatabases\PolarDemo\perpho.xml");
+            foreach (XElement ee in db1.Elements("reflection").Take(100))
+            {
+                Console.WriteLine(ee.ToString());
+            }
+ 
+        }
+
+        private static  Dictionary<string, int> ids = new Dictionary<string, int>();
+        private static int cod = 0;
+        private static int CodeId(string id)
+        {
+            int c = -1;
+            if (!ids.TryGetValue(id, out c))
+            {
+                c = cod;
+                cod++;
+                ids.Add(id, c);
+            }
+            return c;
+        }
+    }
+}
